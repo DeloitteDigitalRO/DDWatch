9cc945a932f4659f2b54c13499d2a1f8fd4f18e3
bogdaniancudd <biancu@deloittece.com>
2019-03-20 18:15:54 +0200

    spring initialzr commit

---
+++ src/main/java/com/deloitte/ddwatch/DdwatchApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.ddwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdwatchApplication.class, args);
+	}
+
+}
